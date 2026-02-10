9c7f7d628887e1d47d8b0559796ff4a4c7f3a658
Hrishik1033 <b25me1033@iitj.ac.in>
2026-02-10 18:10:40 +0530

    Fixed Folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,12 @@
-import { createContext, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Blog from './pages/Blog.jsx'
-import Team from './pages/Team.jsx'
-import Timeline from './pages/Timeline.jsx'
-import Videos from './pages/Videos.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import TopControls from "./assets/components/TopControls";
+import Cards from "./assets/components/Cards/Cards";
+import "./App.css";
 
-export const ThemeContext = createContext({
-  isDarkMode: false,
-  toggleTheme: () => { }
-})
-
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  const toggleTheme = () => setIsDarkMode(t => !t)
-
+export default function App() {
   return (
-    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
-      <div className={isDarkMode ? 'dark' : 'light'}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/timeline" element={<Timeline />} />
-            <Route path="/videos" element={<Videos />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
-    </ThemeContext.Provider>
-  )
+    <div className="app">
+      <TopControls />
+      <Cards />
+    </div>
+  );
 }
-
-export default App

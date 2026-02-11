4c597b6c28401f60a443a5788e1305334be6206f
KB <khanakbansal.1999@gmail.com>
2026-02-11 16:23:08 +0530

    feat:footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { createContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Blog from './pages/Blog.jsx'
-import Team from './pages/Team.jsx'
-import Timeline from './pages/Timeline.jsx'
-import Videos from './pages/Videos.jsx'
-import Header from './components/Header.jsx'
+// import Home from './pages/Home.jsx'
+// import About from './pages/About.jsx'
+// import Blog from './pages/Blog.jsx'
+// import Team from './pages/Team.jsx'
+// import Timeline from './pages/Timeline.jsx'
+// import Videos from './pages/Videos.jsx'
+// import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 
 export const ThemeContext = createContext({
@@ -22,7 +22,7 @@ function App() {
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
       <div className={isDarkMode ? 'dark' : 'light'}>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
@@ -33,7 +33,12 @@ function App() {
             <Route path="/videos" element={<Videos />} />
           </Routes>
           <Footer />
-        </BrowserRouter>
+        </BrowserRouter> */}
+<BrowserRouter>
+  <Footer />
+</BrowserRouter>
+
+
       </div>
     </ThemeContext.Provider>
   )
